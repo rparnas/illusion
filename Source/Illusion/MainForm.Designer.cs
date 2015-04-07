680c2105673c2b342c87ceb334d14c5d27ffbab4
Ryan Parnas <ryan.parnas@gmail.com>
2015-04-07 19:52:05 -0400

    Added basic parsing functionality

---
+++ Source/Illusion/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Illusion
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btn_Load = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // btn_Load
+      // 
+      this.btn_Load.Location = new System.Drawing.Point(13, 226);
+      this.btn_Load.Name = "btn_Load";
+      this.btn_Load.Size = new System.Drawing.Size(75, 23);
+      this.btn_Load.TabIndex = 0;
+      this.btn_Load.Text = "Load...";
+      this.btn_Load.UseVisualStyleBackColor = true;
+      this.btn_Load.Click += new System.EventHandler(this.btn_Load_Click);
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(284, 261);
+      this.Controls.Add(this.btn_Load);
+      this.Name = "MainForm";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btn_Load;
+  }
+}
+
---
+++ Source/Illusion/MainForm.cs
@@ -0,0 +1,122 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+[assembly: AssemblyTitle("Illusion")]
+[assembly: AssemblyProduct("Illusion")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+namespace Illusion
+{
+  public partial class MainForm : Form
+  {
+    List<Block> Blocks;
+
+    [STAThread]
+    public static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new MainForm());
+    }
+
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    void btn_Load_Click(object sender, EventArgs e)
+    {
+      var ofd = new OpenFileDialog();
+      ofd.Filter = "Excel Workbook (*.xlsx)|*.xlsx";
+      if (ofd.ShowDialog() != DialogResult.OK)
+        return;
+
+      var file = Loader.GetXLSX(ofd.FileName, "Time Sheet");
+      if (file == null)
+        return;
+
+      Blocks = new List<Block>();
+      foreach (DataRow row in file.Rows)
+      {
+        var dateStr = row["Date"].ToString().Trim();
+        var startStr = row["Start"].ToString().Trim();
+        var stopStr = row["Stop"].ToString().Trim();
+        var start = DateTime.ParseExact(dateStr + " " + startStr.Replace("a", "AM").Replace("p", "PM"), "M/d/yy h:mmtt", CultureInfo.InvariantCulture);
+        var stop = DateTime.ParseExact(dateStr + " " + stopStr.Replace("a", "AM").Replace("p", "PM"), "M/d/yy h:mmtt", CultureInfo.InvariantCulture);
+        Blocks.Add(new Block
+        {
+          Start = start,
+          Stop = stop,
+          Project = row["Project"].ToString().Trim(),
+          Feature = row["Feature"].ToString().Trim(),
+          Activity = row["Activity"].ToString().Trim()
+        });
+      }
+    }
+  }
+
+  class Block
+  {
+    public DateTime Start;
+    public DateTime Stop;
+    public string Project;
+    public string Feature;
+    public string Activity;
+  }
+
+  class Loader
+  {
+    public static DataTable GetXLSX(string path, string sheetName)
+    {
+      var ep = new ExcelPackage(new FileInfo(path));
+      foreach (var worksheet in ep.Workbook.Worksheets)
+      {
+        if (worksheet.Name == sheetName && worksheet.Dimension != null)
+          return SheetToTable(worksheet);
+      }
+      return null;
+    }
+
+    static DataTable SheetToTable(ExcelWorksheet ws)
+    {
+      var ret = new DataTable(ws.Name);
+
+      var usedCols = new Dictionary<string, int>();
+      for (int col = 1; col <= ws.Dimension.End.Column; col++)
+      {
+        var cell = ws.Cells[1, col];
+        var text = cell.Text.Replace('\n', '_').Replace('\r', '_');
+
+        // Make up a name for columns without text
+        if (string.IsNullOrWhiteSpace(text))
+          text = cell.Address;
+
+        if (!usedCols.ContainsKey(text))
+          usedCols[text] = 0;
+
+        usedCols[text]++;
+        ret.Columns.Add(text + (usedCols[text] == 1 ? "" : usedCols[text].ToString()));
+      }
+
+      for (int row = 2; row <= ws.Dimension.End.Row; row++)
+      {
+        var newRow = ret.NewRow();
+
+        for (int col = 1; col <= ws.Dimension.End.Column; col++)
+          newRow[col - 1] = ws.Cells[row, col].Text;
+
+        ret.Rows.Add(newRow);
+      }
+
+      return ret;
+    }
+  }
+}
